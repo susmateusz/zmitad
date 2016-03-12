60ee782979fe91e16c5248dc0473ea174f2f1d94
Mateusz Sus <sus.mateusz@gmail.com>
2016-03-13 00:46:15 +0100

    Zrobione zadanie 5.

--- Listy zadań/lista_3_Matlab/l03e06.m
+++ Listy zadań/lista_3_Matlab/l03e06.m
@@ -1,8 +1,8 @@
-#!/usr/bin/env octave
+%#!/usr/bin/env octave
 function l03e06
-pkg load statistics;
-pkg load nan;
+%pkg load statistics;
+%pkg load nan;
 f = figure(06);
-disp("Lista 3. Ćwiczenie 06.");
+disp('Lista 3. Ćwiczenie 06.');
 
 waitfor(f);
