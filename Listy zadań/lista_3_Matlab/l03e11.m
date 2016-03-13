81b522bc9fe11cac6dcf4a31f9622d28b7d94bfd
Mateusz Sus <sus.mateusz@gmail.com>
2016-03-13 20:40:55 +0100

    Lista 3. zad 10. i 11.

--- Listy zadań/lista_3_Matlab/l03e11.m
+++ Listy zadań/lista_3_Matlab/l03e11.m
@@ -1,8 +1,16 @@
-#!/usr/bin/env octave
-function l03e11
-pkg load statistics;
-pkg load nan;
-f = figure(11);
-disp("Lista 3. Ćwiczenie 11.");
-
-waitfor(f);
+disp('Lista 3. Ćwiczenie 11.');
+%% wczytywanie danych
+alfa = 0.05;
+fd = fopen('../Dane/pacjenci.csv');
+formatSpec = '%f,%f,%f,%s';
+C = textscan(fd,formatSpec,'HeaderLines',1);
+fclose(fd);
+cukier = sort(C{3});
+%% testowanie
+% h0: rozkład jest normalny
+% h1: rozkład nie jest normalny
+[H, pValue, W] = swtest(cukier, alfa);
+disp('cukier:');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 0, pValue = 0.377057, W = 0.958053
+% wniosek: cukier - rozklad normalny
