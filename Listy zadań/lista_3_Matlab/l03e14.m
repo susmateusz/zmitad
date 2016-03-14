af6fd05ee1171d1110bccdd2e20db18b16ea6a95
Mateusz Sus <sus.mateusz@gmail.com>
2016-03-14 11:28:58 +0100

    lista 3. zad 14. i 15.

---
+++ Listy zadań/lista_3_Matlab/l03e14.m
@@ -0,0 +1,37 @@
+%pkg load statistics;
+%pkg load nan;
+close; clear;
+disp('Lista 3. Ćwiczenie 14.');
+%% wczytywanie danych
+alfa = 0.05;
+fd = fopen('../Dane/absolwenci.csv');
+formatSpec = '%s %s %s %f %s %s';
+C = textscan(fd,formatSpec,'Delimiter',';','HeaderLines',1);
+fclose(fd);
+%% przetwarzanie danych
+data_length = size(C{1},1);
+place_rolnictwo = [];
+place_pedagogika = [];
+for i=1:data_length,
+   if strcmp(C{3}{i},'Rolnictwo'),
+       place_rolnictwo = [place_rolnictwo; C{4}(i)];
+   else
+       place_pedagogika = [place_pedagogika; C{4}(i)];
+   end
+end
+%% testowanie
+% h0: rozkład plac rolnictwa jest normalny
+% h1: rozkład plac rolnictwa nie jest normalny
+[H, pValue, W] = swtest(place_rolnictwo, alfa);
+disp('rolnictwo:');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 1, pValue = 1.20896e-08, W = 0.957191
+% wniosek: rolnictwo - place nie maja rozkladu normalnego
+
+% h0: rozkład plac w pedagogice jest normalny
+% h1: rozkład plac w pedagogice nie jest normalny
+[H, pValue, W] = swtest(place_pedagogika, alfa);
+disp('pedagogika:');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 1, pValue = 3.09407e-10, W = 0.966776
+% wniosek: pedagogika - płace mają rozkladu normalnego
---
+++ Listy zadań/lista_3_Matlab/l03e15.m
@@ -0,0 +1,59 @@
+close; clear;
+disp('Lista 3. Ćwiczenie 15.');
+%% dane
+alfa=0.05;
+%lek
+xi=log([0.135, 23, 0.38, 106, 0.3, 106.2, 52.6, 1.9, 19.5, 5.8, 6.4]);
+%placebo
+yi=log([1.55, 1.15, 0.27, 0.64, 0.125, 105, 25, 0.8, 3.9, 1.2, 0.145]);
+%% testowanie
+% h0: wyniki z uzyciem leku/placebo ma rozkład normalny
+% h1: wyniki z uzyciem leku/placebo nie ma rozkładu normalnego
+%% testy Kołogomorowa-Smirnowa
+[H,P,KSSTAT,CV] = kstest(xi);
+fprintf('KSTEST dla leku:\n');
+fprintf('H = %g, P = %g, KSSTAT = %g, CV = %g\n',H,P,KSSTAT,CV);
+% output: H = 1, P = 0.000283619, KSSTAT = 0.596978, CV = 0.39122
+% wniosek: Rozkład nie jest normalny
+
+[H,P,KSSTAT,CV] = kstest(yi);
+fprintf('KSTEST dla placebo:\n');
+fprintf('H = %g, P = %g, KSSTAT = %g, CV = %g\n',H,P,KSSTAT,CV);
+% output: H = 0, P = 0.777556, KSSTAT = 0.185967, CV = 0.39122
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+%% testy Lillieforsa
+[H,P,LSTAT,CV] = lillietest(xi);
+fprintf('LILLIETEST dla leku:\n');
+fprintf('H = %g, P = %g, LSTAT = %g, CV = %g\n',H,P,LSTAT,CV);
+% output: H = 0, P = 0.5, LSTAT = 0.147622, CV = 0.2515
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+
+[H,P,LSTAT,CV] = lillietest(xi);
+fprintf('LILLIETEST dla placebo:\n');
+fprintf('H = %g, P = %g, LSTAT = %g, CV = %g\n',H,P,LSTAT,CV);
+% output: H = 0, P = 0.5, LSTAT = 0.147622, CV = 0.2515
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+%% testy Shapiro-Wilka
+[H, pValue, W] = swtest(xi, alfa);
+fprintf('SWTEST dla leku:\n');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 0, pValue = 0.344267, W = 0.922982
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+
+[H, pValue, W] = swtest(yi, alfa);
+fprintf('SWTEST dla placebo:\n');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 0, pValue = 0.253474, W = 0.911416
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+%% testy TTEST
+[H,P] = ttest(xi, mean(xi));
+fprintf('TTEST dla leku:\n');
+fprintf('H = %g, P = %g\n',H,P);
+% output: H = 0, P = 1
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
+
+[H,P] = ttest(yi, mean(yi));
+fprintf('TTEST dla leku:\n');
+fprintf('H = %g, P = %g\n',H,P);
+% output: H = 0, P = 1
+% wniosek: hipoteza o normalności rozkładu nie została odrzucona
