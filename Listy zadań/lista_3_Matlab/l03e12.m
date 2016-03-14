192035308a3eaf6376ae314ba8b6107753252f6c
Mateusz Sus <sus.mateusz@gmail.com>
2016-03-14 09:58:59 +0100

    Lista 3. zad 12. i 13.

--- Listy zadań/lista_3_Matlab/l03e12.m
+++ Listy zadań/lista_3_Matlab/l03e12.m
@@ -1,8 +1,18 @@
-#!/usr/bin/env octave
-function l03e12
-pkg load statistics;
-pkg load nan;
-f = figure(12);
-disp("Lista 3. Ćwiczenie 12.");
-
-waitfor(f);
+%pkg load statistics;
+%pkg load nan;
+disp('Lista 3. Ćwiczenie 12.');
+%% wczytywanie danych
+alfa = 0.1;
+fd = fopen('../Dane/zarowki.csv');
+formatSpec = '%f';
+C = textscan(fd,formatSpec,'HeaderLines',1);
+zarowki = C{1};
+fclose(fd);
+%% testowanie
+% h0: rozkład jest normalny
+% h1: rozkład nie jest normalny
+[H, pValue, W] = swtest(zarowki, alfa);
+disp('zarowki:');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 0, pValue = 0.181652, W = 0.892661
+% wniosek: żarówki - zużycie ma rozklad normalny
--- Listy zadań/lista_3_Matlab/l03e13.m
+++ Listy zadań/lista_3_Matlab/l03e13.m
@@ -1,8 +1,18 @@
-#!/usr/bin/env octave
-function l03e13
-pkg load statistics;
-pkg load nan;
-f = figure(13);
-disp("Lista 3. Ćwiczenie 13.");
-
-waitfor(f);
+%pkg load statistics;
+%pkg load nan;
+disp('Lista 3. Ćwiczenie 13.');
+%% wczytywanie danych
+alfa = 0.05;
+fd = fopen('../Dane/kondensatory.csv');
+formatSpec = '%f';
+C = textscan(fd,formatSpec,'HeaderLines',1);
+kondensatory = C{1};
+fclose(fd);
+%% testowanie
+% h0: rozkład jest normalny
+% h1: rozkład nie jest normalny
+[H, pValue, W] = swtest(kondensatory, alfa);
+disp('kondensatory:');
+fprintf('H = %g, pValue = %g, W = %g\n\n',H,pValue,W);
+% output: H = 0, pValue = 0.145563, W = 0.959802
+% wniosek: kondensatory - pojemność ma rozklad normalny
